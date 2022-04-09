2588614953f9d7da813d4a0a8090675f1c787897
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-04-10 02:56:31 +0300

    challenge16

---
+++ challenge16/app/src/main/java/challenge16/data/BinaryNode.java
@@ -0,0 +1,12 @@
+package challenge16.data;
+
+public class BinaryNode<T extends Comparable<T>> extends Node<T> {
+    public BinaryNode(T data) {
+        super(data);
+    }
+
+    @Override
+    public int compareTo(Node<T> o) {
+        return 0;
+    }
+}
